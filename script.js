1635fda010089d9022b989d37a964c094692c6c2
pajwon <pajwonashraf@gmail.com>
2022-01-03 12:40:53 -0800

    button variables

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+//Suits and numbers
 let suits = ['Diamonds', 'Hearts', 'Spades', 'Clubs']
 let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' ]
 
+//create buttons from ids in index.html
+
+let startGameButton = document.querySelector('#start-game')
+
+let hitButton = document.querySelector('#hit')
+
+let stayButton = document.querySelector('#stay')
