b2400f6352eef2263ce2e71f8fd622c5d405c8ca
pajwon <pajwonashraf@gmail.com>
2022-01-03 13:06:58 -0800

    made the deck

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ let deck = []
 //in html just want to show start game not hit and stay yet 
 hitButton.style.display = 'none'
 stayButton.style.display = 'none'
+
+//making a deck using two for loops
+function makeDeck() {
+    let deck = []
+    for (let indexSuits = 0; indexSuits < suits.length; indexSuits++) {
+        for (let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers++) {
+            //this makes one card with random suit and number so make card variable
+            let card = {
+                suit: suits[indexSuits],
+                number: numbers[indexNumbers]
+            }
+            //add the card to deck 
+            deck.push(card);
+        }
+    }
+    return deck
+}
+// check console to see if you made 52 cards, it worked! but in order
+// console.log(makeDeck()) 
